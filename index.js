a61e6958864647c7fb2ea5daa4e0a561ce1f84b9
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-01-09 11:49:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email) {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name, email }),
+  })
+    .then((resp) => resp.json())
+    
+    .then((data) => {
+      const userId = data.id;
+
+      const paragraph = document.createElement("p");
+      paragraph.setAttribute("id", "user-id");
+      paragraph.textContent = `User Id: ${userId}`;
+
+      document.body.append(paragraph);
+    })
+    .catch((error) => {
+      const errorMessage = document.createElement("div");
+      errorMessage.textContent = `Error: ${error.message}`;
+
+      document.body.appendChild(errorMessage);
+    });
+}
+submitData();
